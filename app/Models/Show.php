87c127324bd9bcaf562ba958ebd2b44e3a729b8a
Dirk Rüdiger <dirk@bei3.net>
2020-12-31 21:39:38 +0100

    Added PHPDocs for models

--- app/Models/Show.php
+++ app/Models/Show.php
@@ -5,6 +5,45 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Show
+ *
+ * @property int $id
+ * @property int $station_id
+ * @property string $label
+ * @property string|null $description
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $active
+ * @property int $duration
+ * @property int $day
+ * @property int $hour
+ * @property int $minute
+ * @property string|null $homepage_url
+ * @property string|null $icon_url
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Episode[] $episodes
+ * @property-read int|null $episodes_count
+ * @property-read \App\Models\Station $station
+ * @method static \Illuminate\Database\Eloquent\Builder|Show newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Show newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Show query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereDay($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereDuration($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereHomepageUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereHour($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereIconUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereLabel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereMinute($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereStationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Show whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Show extends Model
 {
     use HasFactory;
--- app/Models/Station.php
+++ app/Models/Station.php
@@ -5,6 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Station
+ *
+ * @property int $id
+ * @property string $label
+ * @property string|null $description
+ * @property string $slug
+ * @property string $homepage_url
+ * @property string|null $icon_url
+ * @property string $stream_url
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Show[] $shows
+ * @property-read int|null $shows_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Station newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Station newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Station query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereHomepageUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereIconUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereLabel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereStreamUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Station whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Station extends Model
 {
     use HasFactory;
